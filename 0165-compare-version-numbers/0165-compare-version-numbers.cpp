d62375781e109c064d9bfce77cc4fbe3620cb172
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2025-12-05 22:44:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.49%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,88 +1,33 @@
 class Solution {
 public:
+    vector<int> parse(string s) {
+        vector<int> ans;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            int j = i;
+            while (j < n && s[j] != '.') {
+                j++;
+            }
+            string tmp = s.substr(i, j - i);
+            ans.push_back(stoi(tmp));
+            i = j + 1;
+        }
+        return ans;
+    }
     int compareVersion(string version1, string version2) {
-        int cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < version1.length(); i++) {
-            if (version1[i] == '.')
-                cnt1++;
-        }
-        for (int i = 0; i < version2.length(); i++) {
-            if (version2[i] == '.')
-                cnt2++;
-        }
-        cnt1++;
-        cnt2++;
-        int sz = max(cnt1, cnt2);
-        vector<int> tmp1, tmp2;
-        int i = 0, n = version1.length();
-        string x = "";
-        while (i < n) {
-            if (version1[i] == '.') {
-                int y = stoi(x);
-                tmp1.push_back(y);
-                i++;
-                x = "";
-            } else {
-                x += version1[i];
-                i++;
-            }
-        }
-        int y = stoi(x);
-        tmp1.push_back(y);
-        x = "";
-        i = 0, n = version2.length();
-        while (i < n) {
-            if (version2[i] == '.') {
-                int y = stoi(x);
-                tmp2.push_back(y);
-                i++;
-                x = "";
-            } else {
-                x += version2[i];
-                i++;
-            }
-        }
-        y = stoi(x);
-        tmp2.push_back(y);
-        x = "";
-        int ans = 0;
-        // case1 => both sizes equal
-        if (cnt1 == cnt2) {
-            for (int i = 0; i < cnt1; i++) {
-                if (tmp1[i] < tmp2[i])
-                    return -1;
-                if (tmp1[i] > tmp2[i])
-                    return 1;
-            }
-            return 0;
-        }
-        // case2 => sz1 > sz2
-        if (cnt1 > cnt2) {
-            int diff = cnt1 - cnt2;
-            for (int i = 0; i < diff; i++) {
-                tmp2.push_back(0);
-            }
-            for (int i = 0; i < cnt1; i++) {
-                if (tmp1[i] < tmp2[i])
-                    return -1;
-                if (tmp1[i] > tmp2[i])
-                    return 1;
-            }
-            return 0;
-        }
-        // case3 => sz1 < sz2
-        if (cnt1 < cnt2) {
-            int diff = cnt2 - cnt1;
-            for (int i = 0; i < diff; i++) {
-                tmp1.push_back(0);
-            }
-            for (int i = 0; i < cnt2; i++) {
-                if (tmp1[i] < tmp2[i])
-                    return -1;
-                if (tmp1[i] > tmp2[i])
-                    return 1;
-            }
-            return 0;
+        vector<int> v1 = parse(version1);
+        vector<int> v2 = parse(version2);
+
+        int m = max(v1.size(), v2.size());
+        v1.resize(m, 0);
+        v2.resize(m, 0);
+
+        for (int i = 0; i < m; i++) {
+            if (v1[i] < v2[i])
+                return -1;
+            if (v1[i] > v2[i])
+                return 1;
         }
         return 0;
     }
