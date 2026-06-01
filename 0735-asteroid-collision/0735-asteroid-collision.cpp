2d53bf60f4be2350bc228ebea2c3069549f2b8f6
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-06-01 20:12:41 +0530

    Time: 0 ms (100%), Space: 22.3 MB (6.35%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n = asteroids.size();
+    vector<int> asteroidCollision(vector<int>& arr) {
         stack<int> st;
+        int n = arr.size();
+        
         int i = 0;
-
+        
         while (i < n) {
-            if (!st.empty() && st.top() > 0 && asteroids[i] < 0) {
-                if (st.top() == abs(asteroids[i])) {
-                    st.pop();
+            if (!st.empty()) {
+                if (st.top() < 0 && arr[i] < 0) {
+                    st.push(arr[i]);
                     i++;
-                } else if (st.top() > abs(asteroids[i])) {
+                }
+                else if (st.top() > 0 && arr[i] > 0) {
+                    st.push(arr[i]);
                     i++;
-                } else if (st.top() < abs(asteroids[i])) {
-                    st.pop();
+                } else if (st.top() > 0 && arr[i] < 0) {
+                    if (abs(st.top()) > abs(arr[i])) {
+                        i++;
+                    } else if (abs(st.top()) < abs(arr[i])) {
+                        st.pop();
+                    } else if (abs(st.top()) == abs(arr[i])) {
+                        st.pop();
+                        i++;
+                    }
+                } else if (st.top() < 0 && arr[i] > 0) {
+                    st.push(arr[i]);
+                    i++;
                 }
             } else {
-                st.push(asteroids[i]);
+                st.push(arr[i]);
                 i++;
             }
         }
-
+        
         vector<int> ans;
         while (!st.empty()) {
             ans.push_back(st.top());
             st.pop();
         }
+        
         reverse(ans.begin(), ans.end());
+        
         return ans;
     }
 };
