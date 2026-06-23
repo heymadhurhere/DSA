08a55483f5fb8b9d86a149682782b7c7aedd0eaa
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-06-23 21:06:38 +0530

    Time: 1124 ms (5%), Space: 68.3 MB (5.62%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,40 +1,58 @@
 class Solution {
 public:
-    int solve(int idx, int last, vector<int>& nums, vector<vector<int>>& dp,
-              vector<vector<int>>& path) {
-        if (idx == nums.size()) {
+    vector<int> ans;
+    int solve(int idx, vector<int>& tmp, int& mini, vector<int>& arr,
+              vector<vector<int>>& dp, int prev) {
+        if ((int)tmp.size() > mini) {
+            ans = tmp;
+            mini = (int)tmp.size();
+        } else if ((int)tmp.size() == mini) {
+            if (tmp > ans) {
+                ans = tmp;
+            }
+        }
+
+        if (idx >= arr.size()) {
             return 0;
         }
-        if (dp[idx][last + 1] != -1) {
-            return dp[idx][last + 1];
+
+        if (dp[idx][prev + 1] != -1 &&
+            (int)tmp.size() + dp[idx][prev + 1] < mini) {
+            return dp[idx][prev + 1];
         }
-        int notTake = solve(idx + 1, last, nums, dp, path);
-        int take = INT_MIN;
-        if (last == -1 || nums[idx] % nums[last] == 0) {
-            take = 1 + solve(idx + 1, idx, nums, dp, path);
-        }
-        if (take > notTake) {
-            path[idx][last + 1] = 1;
-        } else {
-            path[idx][last + 1] = 0;
-        }
-        return dp[idx][last + 1] = max(take, notTake);
-    }
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        vector<vector<int>> path(n, vector<int>(n + 1, -1));
-        int dummy = solve(0, -1, nums, dp, path);
-        int idx = 0, last = -1;
-        vector<int> ans;
-        while (idx < n) {
-            if (path[idx][last + 1] == 1) {
-                ans.push_back(nums[idx]);
-                last = idx;
+
+        int take = INT_MIN, notTake = INT_MIN;
+
+        if (!tmp.empty()) {
+            if (arr[idx] % tmp.back() == 0) {
+                // take
+                tmp.push_back(arr[idx]);
+                take = 1 + solve(idx + 1, tmp, mini, arr, dp, idx);
+                // not take
+                tmp.pop_back();
+                notTake = solve(idx + 1, tmp, mini, arr, dp, prev);
+            } else {
+                notTake = solve(idx + 1, tmp, mini, arr, dp, prev);
             }
-            idx++;
+        } else if (tmp.empty()) {
+            // take
+            tmp.push_back(arr[idx]);
+            take = 1 + solve(idx + 1, tmp, mini, arr, dp, idx);
+            // not take
+            tmp.pop_back();
+            notTake = solve(idx + 1, tmp, mini, arr, dp, prev);
         }
+        return dp[idx][prev + 1] = max(take, notTake);
+    }
+    vector<int> largestDivisibleSubset(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        int mini = INT_MIN;
+        sort(arr.begin(), arr.end());
+        int idx = 0;
+        vector<int> tmp;
+        int prev = -1;
+        solve(idx, tmp, mini, arr, dp, prev);
         return ans;
     }
 };
