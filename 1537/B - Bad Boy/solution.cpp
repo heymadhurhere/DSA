2849f0ce2685e2f75acb053c98299d6952e262be
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:14:48 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(long long n, long long m, long long i, long long j) {
+    // put on opposite corners
+    cout << 1  << " " << m << " " << n << " " << 1 << endl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, m, i, j;
+        cin >> n >> m >> i >> j;
+        solve(n, m, i, j);
+    }
+    return 0;
+}
