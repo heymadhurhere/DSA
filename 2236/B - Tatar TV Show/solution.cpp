75c355012e2332c10bd9b793d4001485d868e8ab
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:42:33 +0530

    Add Tatar TV Show [B] from Codeforces

---
+++ 2236/B - Tatar TV Show/solution.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long, long long>
+#define vi vector<long long>
+#define vpii vector<pii>
+#define all(s) s.begin(), s.end()
+#define sz(s) (ll)(s).size()
+#define F first
+#define S second
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve(string s, ll k) {
+    ll n = s.length();
+    for (ll i = 0; i < n - k; i++) {
+        if (s[i] == '1') {
+            s[i] = '0';
+            s[i + k] = (s[i + k] == '1' ? '0' : '1');
+        }
+    }
+ 
+    for (ll i = 0; i < n; i++) {
+        if (s[i] == '1') {
+            cout << "NO
+";
+            return;
+        }
+    }
+    cout << "YES
+";
+    return;
+}
+ 
+ 
+int main() {
+ 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+ 
+    while (t--) {
+        ll n, k;
+        cin >>n >> k;
+        string s;
+        cin >> s;
+        solve(s, k);
+    }
+    return 0;
+}
