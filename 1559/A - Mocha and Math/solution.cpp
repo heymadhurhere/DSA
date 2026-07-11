b31137b7ead6e0989a30189ee962827b2a9146a2
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:14:36 +0530

    Add Mocha and Math [A] from Codeforces

---
+++ 1559/A - Mocha and Math/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int solve(vector<int>& arr) {
+   int n = arr.size();
+   int ans = arr[0];
+   for (int i = 1; i < n; i++) {
+    ans &= arr[i];
+   }
+   return ans;
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cout << solve(arr) << endl;
+    }
+    return 0;
+}
