063c27abedec8d3dc9a9ec13fa333b7f4101a865
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:11:29 +0530

    Add Make It Zero [A] from Codeforces

---
+++ 1869/A - Make It Zero/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(vector<int>& arr) {
+    int n = arr.size();
+    if (n % 2 == 0) {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    }
+    else {
+        cout << 4 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    }
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        solve(arr);
+    }
+    return 0;
+}
