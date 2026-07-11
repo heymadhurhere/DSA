b0cd7a1d5422dc15f8eb02434ec664a0e27f178a
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:32:10 +0530

    Add Shifted MEX [C] from Codeforces

---
+++ 2185/C - Shifted MEX/solution.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long, long long>
+#define vi vector<long long>
+#define vpii vector<pii>
+#define all(s) s.begin(), s.end()
+#define sz(s) (ll)(s).size()
+#define F first
+#define S second
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve (vi &a) {
+    ll n = sz(a);
+    sort(all(a));
+    ll b = 1, cur = 1;
+ 
+    for (ll i = 1; i < n; i++) {
+        if (a[i] == a[i - 1]) {
+            continue;
+        }
+        if (a[i] == a[i - 1] + 1) {
+            cur++;
+        } else {
+            cur = 1;
+        }
+        b = max(b, cur);
+    }
+    cout << b;
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        vi a(n);
+        for (ll i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        solve(a);
+        cout << "
+";
+    }
+    return 0;
+}
