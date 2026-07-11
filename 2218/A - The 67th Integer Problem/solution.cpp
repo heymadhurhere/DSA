07a20660ea12ba3a3f9e48b883cc8b245506a9d6
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:32:47 +0530

    Add The 67th Integer Problem [A] from Codeforces

---
+++ 2218/A - The 67th Integer Problem/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long, long long>
+#define vi vector<long long>
+#define vpii vector<pii>
+#define all(s) s.begin(), s.end()
+#define sz(s) (ll)(s).size()
+#define F first
+#define S second
+const ll MOD = 1e9 + 7;
+ 
+ll solve(ll x) {
+    return x;
+}
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll x;
+        cin >> x;
+        cout << solve(x) << endl;
+    }
+    return 0;
+}
