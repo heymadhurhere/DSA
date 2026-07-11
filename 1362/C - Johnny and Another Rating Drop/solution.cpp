ec9b469e18f405ddd4f141e3e2882de4776fb87d
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:43:38 +0530

    Add Johnny and Another Rating Drop [C] from Codeforces

---
+++ 1362/C - Johnny and Another Rating Drop/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long, long long>
+#define vi vector<long long>
+#define vpii vector<pii>
+#define all(s) s.begin(), s.end()
+#define sz(s) (ll)(s).size()
+#define F first
+#define S second
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve(ll n) {
+    ll ans = 2 * n - __builtin_popcountll(n);
+    cout << ans << "
+";
+    return;
+}
+ 
+ 
+int main() {
+ 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+ 
+    while (t--) {
+        ll n;
+        cin >> n;
+        solve(n);
+    }
+    return 0;
+}
