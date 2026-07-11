763f610dc87dd1421d6eb3a5ad9c6fcbcc52d47c
Madhur Anand <162946777+heymadhurhere@users.noreply.github.com>
2026-07-11 09:32:04 +0530

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long, long long>
+#define vi vector<long long>
+#define vpii vector<pii>
+#define all(s) s.begin(), s.end()
+#define sz(s) (ll)(s).size()
+#define F first
+#define S second
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve (vi &a) {
+    ll n = sz(a);
+    ll tmp = *max_element(all(a));
+    cout << tmp * n ;
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        vi a(n);
+        for (ll i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        solve(a);
+        cout << "
+";
+    }
+    return 0;
+}
